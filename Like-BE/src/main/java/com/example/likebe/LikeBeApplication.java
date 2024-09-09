20e09ed6d70315cbcfbb67f13568d40d820694eb
C0823G1-nguyentrucvi <nguyentrucvi10112004@gmail.com>
2024-09-09 16:00:54 +0700

    test

---
+++ Like-BE/src/main/java/com/example/likebe/LikeBeApplication.java
@@ -0,0 +1,15 @@
+package com.example.likebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeBeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LikeBeApplication.class, args);
+
+	}
+
+}
---
+++ Like-BE/src/test/java/com/example/likebe/LikeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
